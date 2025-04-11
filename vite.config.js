52ed3f2a44200d258c0b37ca80d0175d93acc65f
MarsiKris76 <bykovme@krista.ru>
2025-04-12 00:36:05 +0300

    Исправил сборку, добавил команду старт и настройку для запуска на порту 3000. Внёс исправления в макеты.

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,17 @@ import handlebars from 'vite-plugin-handlebars';
 export default defineConfig({
     root: resolve(__dirname, '.'),
     build: {
+        rollupOptions: {
+            input: {
+                index: resolve(__dirname, 'index.html'),
+                error_404: resolve(__dirname, './src/pages/404_error.html'),
+                error_500: resolve(__dirname, './src/pages/500_error.html'),
+                chat_list: resolve(__dirname, './src/pages/chat_list.html'),
+                login: resolve(__dirname, './src/pages/login.html'),
+                profile: resolve(__dirname, './src/pages/profile.html'),
+                registration: resolve(__dirname, './src/pages/registration.html'),
+            }
+        },
         outDir: resolve(__dirname, 'dist'),
     },
     plugins: [handlebars({
